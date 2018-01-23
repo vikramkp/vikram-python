b0970ef7e0660fbdc403465de671767349fb6dfd
vikramkp <35484758+vikramkp@users.noreply.github.com>
2018-01-23 15:08:49 +0530

    Add files via upload

---
+++ ifelse.py
@@ -0,0 +1,7 @@
+#program to check number postive or negative
+a=int(input("enter number to check positive or negative"))
+if a>0:
+      print(a,"is a positve")
+else:
+      print(a,"is a negative")
+      
