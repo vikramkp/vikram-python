76ffdd6197460c907c944a068a8057fb1b0e5540
vikramkp <35484758+vikramkp@users.noreply.github.com>
2018-01-30 15:26:07 +0530

    Add files via upload

---
+++ program7.py
@@ -0,0 +1,5 @@
+#and operator
+if 1==1 and 2!=2:
+    print("true")
+else:
+    print("false")
---
+++ program8.py
@@ -0,0 +1,10 @@
+#nested statement
+n=7
+if n>3:
+    print("3")
+    if n<5:
+        print("5")
+        if n==7:
+            print("7")
+    
+        
